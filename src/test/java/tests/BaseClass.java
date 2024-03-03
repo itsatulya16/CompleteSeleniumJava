e9b20119476c02744f5a25207739e6dd015c6511
atul ambade <atulambadepatil@gmail.com>
2024-03-03 12:19:32 +0530

    added WebDriverManager code

---
+++ src/test/java/tests/BaseClass.java
@@ -0,0 +1,49 @@
+package tests;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+    public static WebDriver driver;
+    static ExtentReports report;
+    public static ExtentTest test;
+    static ExtentReports extent = new ExtentReports();
+    @BeforeMethod
+    public static void setUp(){
+
+//        String path = System.getProperty("user.dir") + "\\driver\\chromedriver.exe";
+//        System.setProperty("webdriver.chrome.driver", path);
+
+        // following code is using webdriver manager
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+        extent.attachReporter(spark);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    void cleanUp(){
+        extent.flush();
+        driver.quit();
+    }
+
+    // TODO fix that method
+//    public static String capture(WebDriver driver)throws IOException {
+//        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//        File dest = new File("src/../ExceImages/" + System.currentTimeMillis() + ".png");
+//        String errlpath = dest.getAbsolutePath();
+//        FileUtils.copyFile(scrFile, dest);
+//                return errlpath;
+//    }
+}
--- src/test/java/tests/seleniumTest.java
+++ src/test/java/tests/seleniumTest.java
@@ -1,49 +1,11 @@
 package tests;
 
 import Pages.HomePage;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
-
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-public class seleniumTest {
-    public static WebDriver driver;
-    static ExtentReports report;
-    public static ExtentTest test;
-    static ExtentReports extent = new ExtentReports();
-    @BeforeMethod
-    public static void setUp(){
-        String path = System.getProperty("user.dir") + "\\driver\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver", path);
-        driver = new ChromeDriver();
-        driver.get("https://www.facebook.com/");
-        ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
-        extent.attachReporter(spark);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-    }
-
-
-    // TODO fix that method
-//    public static String capture(WebDriver driver)throws IOException {
-//        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-//        File dest = new File("src/../ExceImages/" + System.currentTimeMillis() + ".png");
-//        String errlpath = dest.getAbsolutePath();
-//        FileUtils.copyFile(scrFile, dest);
-//                return errlpath;
-//    }
-
+public class seleniumTest extends BaseClass {
     @Test
-    void test1(){
+    void test1() {
         test = extent.createTest("validate first test field", "so beautifully validated");
         HomePage.setUsername();
         HomePage.setPassWord();
@@ -51,7 +13,7 @@ public class seleniumTest {
     }
 
     @Test
-    void test2(){
+    void test2() {
         test = extent.createTest("validate second test field", "so beautifully validated");
         HomePage.setUsername();
         HomePage.setPassWord();
@@ -59,16 +21,10 @@ public class seleniumTest {
     }
 
     @Test
-    void test3(){
+    void test3() {
         test = extent.createTest("validate third test field", "so beautifully validated");
         HomePage.setUsername();
         HomePage.setPassWord();
 
-    }
-
-    @AfterMethod
-    void cleanUp(){
-        extent.flush();
-        driver.quit();
     }
 }
