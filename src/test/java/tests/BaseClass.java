59cb6d37e688b7b02631bb3bf62ccce61b4006d8
atul ambade <atulambadepatil@gmail.com>
2024-05-11 20:36:38 +0530

    pom

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -3,6 +3,7 @@ package tests;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -18,12 +19,12 @@ public class BaseClass {
     @BeforeMethod
     public static void setUp(){
 
-        String path = System.getProperty("user.dir") + "\\driver\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver", path);
+//        String path = System.getProperty("user.dir") + "\\driver\\chromedriver.exe";
+//        System.setProperty("webdriver.chrome.driver", path);
 
 //TODO
-//          following code is using webdriver manager
-//        WebDriverManager.chromedriver().clearDriverCache().setup();
+       //   following code is using webdriver manager
+        WebDriverManager.chromedriver().clearDriverCache().setup();
 
         driver = new ChromeDriver();
         driver.get("https://www.ebay.com/");
