df100948c150834cda7a74adfc7bb5f477ff5b64
atul ambade <atulambadepatil@gmail.com>
2025-10-03 16:16:02 +0530

    added logger

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -4,68 +4,89 @@ import DB_Testing.TestLogger;
 import Utils.Utility;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Allure;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+
+import org.slf4j.LoggerFactory;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
+
 import java.net.URL;
+import java.time.Duration;
 
 public class BaseClass {
+
     public static WebDriver driver;
 
+    private static final Logger log = LogManager.getLogger(BaseClass.class);
+
     @BeforeMethod
-    public void setUp() throws MalformedURLException {
-       try {
-           System.setProperty("webdriver.gecko.driver", "/snap/bin/geckodriver");
+    public void setUp() {
 
-           FirefoxOptions options = new FirefoxOptions();
-           options.addArguments("--headless");
-           options.addArguments("--disable-notifications");
-           options.addArguments("--disable-popup-blocking");
+        try {
+            //to run in docker container --------------------------------------
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("chrome");
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
 
-           driver = new FirefoxDriver(options);
-       } catch (Exception e) {
-           System.out.println("Error in setting up Firefox driver: " + e.getMessage());
-           System.out.println("Switching to Chrome driver");
-       }
-       try {
-           WebDriverManager.chromedriver().clearDriverCache().setup();
-           ChromeOptions options = new ChromeOptions();
-           options.addArguments("--disable-notifications");
-           options.addArguments("--disable-popup-blocking");
-           options.addArguments("--headless");
-           driver = new ChromeDriver(options);
-           System.out.println("Chrome driver setup successfully");
-       } catch (Exception e) {
-           System.out.println("Error in setting up Chrome driver: " + e.getMessage());
-       }
+        } catch (Exception e) {
 
-       driver.manage().window().maximize();
-       driver.get("https://www.facebook.com/");
+            log.warn("docker is not ready{}", e.getMessage());
 
-         //to run in docker container --------------------------------------
-//         DesiredCapabilities capabilities = new DesiredCapabilities();
-//         capabilities.setBrowserName("chrome");
-//         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
-//         driver.manage().window().maximize();
-//         driver.get("https://www.facebook.com/");
+
+
+            try {
+                System.setProperty("webdriver.gecko.driver", "/snap/bin/geckodriver");
+
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("--headless");
+                options.addArguments("--disable-notifications");
+                options.addArguments("--disable-popup-blocking");
+
+                driver = new FirefoxDriver(options);
+            } catch (Exception f) {
+
+                log.warn("Error in setting up Firefox driver: {}", f.getMessage());
+
+                log.info("Switching to Chrome driver");
+                try {
+                    WebDriverManager.chromedriver().clearDriverCache().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--disable-notifications");
+                    options.addArguments("--disable-popup-blocking");
+                    options.addArguments("--headless");
+                    driver = new ChromeDriver(options);
+
+                    log.info("Chrome driver setup successfully");
+                } catch (Exception d) {
+
+                    log.warn("Error in setting up Chrome driver: {}", d.getMessage());
+                }
+            }
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+            driver.manage().window().maximize();
+            driver.get("https://www.facebook.com/");
+
+        }
+
     }
 
-    @BeforeTest
-    public void clearDBLogs() {
-        TestLogger.clearTestResults();
-    }
+
+
+//    @BeforeTest
+//    public void clearDBLogs() {
+//        TestLogger.clearTestResults();
+//    }
 
     // @IMP : following code is for attaching screenshot to allure report when test case failed
     @AfterMethod
@@ -83,13 +104,16 @@ public class BaseClass {
             System.out.println("Test skipped: " + result.getName());
 
         }
-        // follwing code will log the test result logs in DB
+        // follwing code will log the test result logs in DB---------------------------------
 
-        TestLogger.logTestResult(result.getName(), status, (result.getEndMillis() - result.getStartMillis()) / 1000.0);
+       // TestLogger.logTestResult(result.getName(), status, (result.getEndMillis() - result.getStartMillis()) / 1000.0);
+
         if (driver != null) {
             driver.quit();
         }
     }
+
+
 
 //    @Attachment(value = "Screenshot on Failure", type = "image/png")
 //    public byte[] saveScreenShot() {
